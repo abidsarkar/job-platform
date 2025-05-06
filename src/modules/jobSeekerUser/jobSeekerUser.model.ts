52ebc48d01029b195fdd8884f69b33420f2055af
abidsarkar <abidsarkar0@gmail.com>
2025-05-06 13:53:59 +0600

    created at updated at

--- src/modules/jobSeekerUser/jobSeekerUser.model.ts
+++ src/modules/jobSeekerUser/jobSeekerUser.model.ts
@@ -9,38 +9,41 @@ import {
 } from "./jobSeekerUser.interface"; // Import IUser Interface
 
 // Job Information Schema
-const jobSeekerPersonalSchema = new Schema<IJobSeekerPersonal>({
-  jobTitle: { type: String,default:null, required: false },
-  experience: { type: String,default:null, required: false },
-  education: { type: String,default:null, required: false },
-  personalWebsite: { type: String,default:null, required: false },
-  profilePicture: {
-    filePathURL: {
-      type: String,
-      default: "/uploads/profile_pictures/1745471655982-763482898.jpg",
-      required: false,
+const jobSeekerPersonalSchema = new Schema<IJobSeekerPersonal>(
+  {
+    jobTitle: { type: String, default: null, required: false },
+    experience: { type: String, default: null, required: false },
+    education: { type: String, default: null, required: false },
+    personalWebsite: { type: String, default: null, required: false },
+    profilePicture: {
+      filePathURL: {
+        type: String,
+        default: "/uploads/profile_pictures/1745471655982-763482898.jpg",
+        required: false,
+      },
+      fileOriginalName: {
+        type: String,
+        default: "defaultProfilePictureAADD.jpg",
+        required: false,
+      },
+      fileServerName: {
+        type: String,
+        default: "1745471655982-763482898.jpg",
+        required: false,
+      },
+      pathA: { type: String, required: false },
     },
-    fileOriginalName: {
-      type: String,
-      default: "defaultProfilePictureAADD.jpg",
-      required: false,
+    resume: {
+      fileOriginalName: { type: String, required: false },
+      fileServerName: { type: String, required: false },
+      filePathURL: { type: String, required: false },
+      pathA: { type: String, required: false },
     },
-    fileServerName: {
-      type: String,
-      default: "1745471655982-763482898.jpg",
-      required: false,
-    },
-    pathA: { type: String, required: false },
+    userId: { type: mongoose.Types.ObjectId, ref: "generalUser" }, // Reference to the JobSeekerUser model
+    email: { type: String, required: false },
   },
-  resume: {
-    fileOriginalName: { type: String, required: false },
-    fileServerName: { type: String, required: false },
-    filePathURL: { type: String, required: false },
-    pathA: { type: String, required: false },
-  },
-  userId: { type: mongoose.Types.ObjectId, ref:"generalUser"}, // Reference to the JobSeekerUser model
-  email: { type: String, required: false },
-});
+  { timestamps: true }
+);
 
 const JobSeekersPersonal = mongoose.model<IJobSeekerPersonal>(
   "JobSeekersPersonal",
@@ -49,17 +52,17 @@ const JobSeekersPersonal = mongoose.model<IJobSeekerPersonal>(
 
 // Personal Information Schema
 const JobSeekersProfileSchema = new Schema<IJobSeekerProfile>({
-  nationality: { type: String,default:null, required: false },
-  dob: { type: Date, default:null, required: false },
+  nationality: { type: String, default: null, required: false },
+  dob: { type: Date, default: null, required: false },
   gender: { type: String, enum: Object.values(Gender), required: false },
   maritalStatus: {
     type: String,
     enum: Object.values(MaritalStatus),
-    default:null, 
+    default: null,
     required: false,
   },
-  biography: { type: String, default:null, required: false },
-  userId: { type: mongoose.Types.ObjectId, ref:"generalUser"}, // Reference to the JobSeekerUser model
+  biography: { type: String, default: null, required: false },
+  userId: { type: mongoose.Types.ObjectId, ref: "generalUser" }, // Reference to the JobSeekerUser model
   email: { type: String, required: false },
 });
 
@@ -69,14 +72,14 @@ const JobSeekersProfile = mongoose.model<IJobSeekerProfile>(
 );
 
 const JobSeekersAccountSettingSchema = new Schema<IJobSeekerAccountSettings>({
-  fullAddress: { type: String, default:null, required: false },
-  city: { type: String,default:null,  required: false },
-  state: { type: String,default:null,  required: false },
-  country: { type: String,default:null, required: false },
-  phoneNumber: { type: String,default:null,  required: false },
-  jobAlertRole: { type: String,default:null,  required: false },
-  jobAlertCity: { type: String,default:null,  required: false },
-  userId: { type: mongoose.Types.ObjectId, ref:"generalUser"},// Reference to the JobSeekerUser model
+  fullAddress: { type: String, default: null, required: false },
+  city: { type: String, default: null, required: false },
+  state: { type: String, default: null, required: false },
+  country: { type: String, default: null, required: false },
+  phoneNumber: { type: String, default: null, required: false },
+  jobAlertRole: { type: String, default: null, required: false },
+  jobAlertCity: { type: String, default: null, required: false },
+  userId: { type: mongoose.Types.ObjectId, ref: "generalUser" }, // Reference to the JobSeekerUser model
   email: { type: String, required: false },
 });
 const JobSeekersAccountSetting = mongoose.model<IJobSeekerAccountSettings>(
@@ -86,11 +89,11 @@ const JobSeekersAccountSetting = mongoose.model<IJobSeekerAccountSettings>(
 
 // Social Media Schema
 const JobSeekersSocialMediaSchema = new Schema<IJobSeekerSocialMedia>({
-  facebookLink: { type: String,default:null,  required: false },
-  xLink: { type: String, default:null, required: false },
-  instagramLink: { type: String,default:null,  required: false },
-  linkedinLink: { type: String,default:null,  required: false },
-  userId: { type: mongoose.Types.ObjectId, ref:"generalUser"},// Reference to the JobSeekerUser model
+  facebookLink: { type: String, default: null, required: false },
+  xLink: { type: String, default: null, required: false },
+  instagramLink: { type: String, default: null, required: false },
+  linkedinLink: { type: String, default: null, required: false },
+  userId: { type: mongoose.Types.ObjectId, ref: "generalUser" }, // Reference to the JobSeekerUser model
   email: { type: String, required: false },
 });
 
